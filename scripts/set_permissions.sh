c72f40f1b5d76595e6bbd4e2667cd1aaf0635fda
tirumalabhairi <tirumala.b@crimsonlaketech.com>
2024-10-02 15:07:32 +0530

    update user

--- scripts/set_permissions.sh
+++ scripts/set_permissions.sh
@@ -1,4 +1,8 @@
-chown -R ubuntu:ubuntu /var/www/html/laravel
+chown -R :www-data /var/www/html/laravel
 chmod -R 755 /var/www/html/laravel
-chown -R ubuntu:ubuntu /var/www/html/laravel/storage
+chown -R 755 www-data:www-data /var/www/html/laravel/storage
 chmod -R 775 /var/www/html/laravel/storage /var/www/html/laravel/bootstrap/cache
+
+
+chown -R :www-data /var/www/html/laravel
+chmod -R 775 /var/www/html/laravel

1b51f55aa81078f0d18434baa5276b2e0add8d39
tirumala18 <59111355+tirumala18@users.noreply.github.com>
2024-10-02 12:11:49 +0530

    update scripts

--- scritps/set_permissions.sh
+++ scritps/set_permissions.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo chown -R www-data:www-data /var/www/laravel
-sudo chmod -R 755 /var/www/laravel/storage
-sudo chmod -R 755 /var/www/laravel/bootstrap/cache
+sudo chown -R www-data:www-data /var/www/html/laravel
+sudo chmod -R 755 /var/www/html//laravel/storage
+sudo chmod -R 755 /var/www/html/laravel/bootstrap/cache
